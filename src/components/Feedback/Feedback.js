d6971378d539ab38716485f31970841760e2173a
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-11-23 00:38:25 +0100

    feedbackHooks

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,93 +1,55 @@
-import React from "react";
+import { useState } from "react";
 import Controler from "../Controler/Controler";
 import Statistics from "../Statistics/Statistics";
 import Notification from "../Notification/Notification";
 import Section from "../Section/Section";
 
-class Feedback extends React.Component {
+function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const handleIncrementGood = () => {
+    setGood((prevs) => prevs + 1)
   };
-
-  handleIncrementGood = () => {
-    this.setState((prevState) => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
+  const handleIncrementNeutral = () => {
+    setNeutral((prevs) => prevs + 1);
   };
+   const handleIncrementBad = () => {
+     setBad((prevs) => prevs + 1);
+   };
 
-  handleIncrementNeutral = () => {
-    this.setState((prevState) => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
-  };
+  const countTotalFeedback = () => good + neutral + bad;
 
-  handleIncrementBad = () => {
-    this.setState((prevState) => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  };
-  // Процент положительных отзывов компании (отображается на странице отзывов о компании на портале) рассчитывается по следующей формуле:
-  // Z = Х / Y * 100%, где:
-  //  Z― процент положительных отзывов;
-  // Х = количество только положительных отзывов (отлично и хорошо) о компании за последние 365 дней;
-  // Y = сумма положительных и негативных отзывов
-
-  // Метод ----Math.round---- выполняет округление до ближайшего целого числа по правилам математического округления.
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const pozitiv = Math.round((this.state.good * 100) / total);
-    return pozitiv;
-  };
-
+  const countPositiveFeedbackPercentage = () => 
+    Math.round((good * 100) / totalFeedback);
   
 
-  render() {
-    const { handleIncrementGood, handleIncrementBad, handleIncrementNeutral } = this;
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const pozitiv = this.countPositiveFeedbackPercentage();
-    return (
-      <div>
-        
-        <Section title='Please leave feedback'>
-          <Controler
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedback = countPositiveFeedbackPercentage();
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <Controler
           onIncrementGood={handleIncrementGood}
           onIncrementNeutral={handleIncrementNeutral}
           onIncrementBad={handleIncrementBad}
         />
-        {total === 0 ? (
-          <Notification message='No feedback given' />
-          
+        {totalFeedback === 0 ? (
+          <Notification message="No feedback given" />
         ) : (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          pozitiv={pozitiv}
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            pozitiv={positiveFeedback}
           />
         )}
-          </Section>
-          
-      </div>
-    );
-  }
+      </Section>
+    </div>
+  );
 }
 
 export default Feedback;
---
+++ src/components/Feedback/Feedback1.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Controler from "../Controler/Controler";
+import Statistics from "../Statistics/Statistics";
+import Notification from "../Notification/Notification";
+import Section from "../Section/Section";
+
+class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrementGood = () => {
+    this.setState((prevState) => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  handleIncrementNeutral = () => {
+    this.setState((prevState) => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+
+  handleIncrementBad = () => {
+    this.setState((prevState) => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+  // Процент положительных отзывов компании (отображается на странице отзывов о компании на портале) рассчитывается по следующей формуле:
+  // Z = Х / Y * 100%, где:
+  //  Z― процент положительных отзывов;
+  // Х = количество только положительных отзывов (отлично и хорошо) о компании за последние 365 дней;
+  // Y = сумма положительных и негативных отзывов
+
+  // Метод ----Math.round---- выполняет округление до ближайшего целого числа по правилам математического округления.
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const pozitiv = Math.round((this.state.good * 100) / total);
+    return pozitiv;
+  };
+
+  render() {
+    const { handleIncrementGood, handleIncrementBad, handleIncrementNeutral } =
+      this;
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const pozitiv = this.countPositiveFeedbackPercentage();
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <Controler
+            onIncrementGood={handleIncrementGood}
+            onIncrementNeutral={handleIncrementNeutral}
+            onIncrementBad={handleIncrementBad}
+          />
+          {total === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              pozitiv={pozitiv}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
